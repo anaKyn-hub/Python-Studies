f9e1cda8dcb76010cf4d51ec6d97bf5fa2c5a2bb
anaKyn <anakyndeveloper@gmail.com>
2025-07-14 22:07:06 -0300

    melhorias no código

--- pycodes/adivinhaçãonum.py
+++ pycodes/adivinhaçãonum.py
@@ -1,22 +1,38 @@
 import os
 from random import randint
-#PODER JOGAR NOVAMENTE
+
+randnum = randint(1,5)
+exit = 0
+tentativas = 0
 
 def clear ():
   os.system('cls' if os.name == 'nt' else 'clear')
 
 clear()
 print ("Olá jogador, você consegue advinhar meu número?")
+print ("Para sair do jogo, digite 0")
 
 while True:
   try:
-    num = int(input("Por favor insira um número: "))
-    break
+    num = int(input("Por favor insira um número de 1 a 5: "))
+    tentativas += 1
+
+    while num != randnum and num != exit:
+      clear()
+      print("Errou :(")
+      print("Se quiser sair digite 0, ou continue tentando :)")
+      num = int(input("Tente novamente um número de 1 a 5: "))
+      tentativas += 1
+    
+    if num == randnum:
+      print("\nSucesso! Você acertou o número!")
+      print(f"Você acertou em {tentativas} tentativa!" if tentativas == 1 else f"Você acertou em {tentativas} tentativas!")
+      break
+
+    if num == exit:
+      print("\nObrigada por jogar!")
+      break
+
   except:
     clear() #limpa o terminal
     print("Valor invalido!")
-
-if num == randint(0,5):
-  print("Sucesso!")
-else:
-  print("Falhou :(")
